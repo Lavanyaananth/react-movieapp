bdd0934991fd8a8c4474221d1731d60c9e4c635f
lavanyaananth <lavanyaanand.11@gmail.com>
2019-09-06 19:37:22 +0530

    adding app shell

---
+++ src/components/elements/MovieThumb/MovieThumb.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './MovieThumb.css';
+
+const MovieThumb = () =>{
+    return(
+        <div>
+            Movoe Thumb
+        </div>
+    )
+}
+
+export default MovieThumb;
