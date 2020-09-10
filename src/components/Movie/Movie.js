9d7e4918fa558507d54fd0c543bb131f46df25e4
Lavanya <lavanyaanand.11@gmail.com>
2020-09-10 23:15:18 +0530

    tweaks movie thumb

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -17,17 +17,22 @@ class Movie extends Component {
   }
 
   componentDidMount() {
-    this.setState({ loading: true })
-    // First fetch the movie ...
-    const endpoint = `${API_URL}movie/${this.props.match.params.movieId}?api_key=${API_KEY}&language=en-US`;
-    this.fetchItems(endpoint);
+    if (localStorage.getItem(`${this.props.match.params.movieId}`)) {
+      const state = JSON.parse(localStorage.getItem(`${this.props.match.params.movieId}`));
+      this.setState({ ...state });
+    } else {
+      this.setState({ loading: true })
+      // First fetch the movie ...
+      const endpoint = `${API_URL}movie/${this.props.match.params.movieId}?api_key=${API_KEY}&language=en-US`;
+      this.fetchItems(endpoint);
+    }
   }
 
   fetchItems = (endpoint) => {
     fetch(endpoint)
     .then(result => result.json())
     .then(result => {
-    
+
       if (result.status_code) {
         this.setState({ loading: false });
       } else {
@@ -43,6 +48,8 @@ class Movie extends Component {
               actors: result.cast,
               directors,
               loading: false
+            }, () => {
+              localStorage.setItem(`${this.props.match.params.movieId}`, JSON.stringify(this.state));
             })
           })
         })
--- src/components/elements/MovieThumb/MovieThumb.js
+++ src/components/elements/MovieThumb/MovieThumb.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import PropTypes from 'prop-types';
 import './MovieThumb.css';
 
 const MovieThumb = (props) =>{
@@ -16,5 +17,9 @@ const MovieThumb = (props) =>{
         </div>
     )
 }
-
+MovieThumb.propTypes ={
+  image: PropTypes.string,
+  movieId :PropTypes.number,
+  movieName: PropTypes.string
+}
 export default MovieThumb;
